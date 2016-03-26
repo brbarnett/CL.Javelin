625f6f2c8275c06f6b812edd415c00338c7e9f73
brbarnett <barnett@echosolutionsgroup.com>
2016-03-26 10:20:29 -0500

    Adding untracked files

---
+++ CL.Javelin.Fulfillment.Service/RootRoutes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Nancy;
+
+namespace CL.Javelin.Fulfillment.Service
+{
+    public class RootRoutes : NancyModule
+    {
+        public RootRoutes()
+        {   
+            base.Get["/fulfillment/getOpenRequests", true] = async (x, ct) =>
+            {
+                var openFreightRequests = await Core.Utilities.Http.Get<List<Core.Domain.Freight.Request>>("http://127.0.0.1:9000/freight/openRequests");
+
+                return base.Response.AsJson(openFreightRequests);
+            };
+        }
+    }
+}
