64e445faa38869a8213644a97e9fa686e97abb7a
brbarnett <barnett@echosolutionsgroup.com>
2016-03-26 14:11:06 -0500

    Project name change

--- CL.Javelin.Sales.Client/Properties/AssemblyInfo.cs
+++ CL.Javelin.Sales.Client/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CL.Javeliln.Sales.Client")]
+[assembly: AssemblyTitle("CL.Javelin.Sales.Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CL.Javeliln.Sales.Client")]
+[assembly: AssemblyProduct("CL.Javelin.Sales.Client")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
