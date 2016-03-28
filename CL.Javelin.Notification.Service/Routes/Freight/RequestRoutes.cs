9ead58971f6eb30d4ea4d090062ea91ecb90b40c
brbarnett <barnett@echosolutionsgroup.com>
2016-03-28 11:31:26 -0500

    Missed routing files

---
+++ CL.Javelin.Notification.Service/Routes/Freight/RequestRoutes.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace CL.Javelin.Notification.Service.Routes.Freight
+{
+    public class RequestRoutes : NancyModule
+    {
+        private readonly IHubContext _notificationHubContext;
+
+        private const string BaseUrl = "/freight/requests";
+
+        public RequestRoutes()
+        {
+            this._notificationHubContext = GlobalHost.ConnectionManager.GetHubContext<NotificationHub>();
+
+            base.Post[$"{BaseUrl}/created"] = this.RequestCreated;
+            base.Post[$"{BaseUrl}/updated"] = this.RequestUpdated;
+            base.Post[$"{BaseUrl}/deleted"] = this.RequestDeleted;
+        }
+
+        private async Task<dynamic> RequestCreated(dynamic parameters, CancellationToken ct)
+        {
+            Console.WriteLine($"{base.Request.Method}: {base.Request.Url.Path}");
+
+            var request = this.Bind<Core.Domain.Freight.Request>();
+
+            this._notificationHubContext.Clients.All.push("Created", request);
+
+            return base.Response.AsJson(new { Success = true });
+        }
+
+        private async Task<dynamic> RequestUpdated(dynamic parameters, CancellationToken ct)
+        {
+            Console.WriteLine($"{base.Request.Method}: {base.Request.Url.Path}");
+
+            var request = this.Bind<Core.Domain.Freight.Request>();
+
+            this._notificationHubContext.Clients.All.push("Updated", request);
+
+            return base.Response.AsJson(new { Success = true });
+        }
+
+        private async Task<dynamic> RequestDeleted(dynamic parameters, CancellationToken ct)
+        {
+            Console.WriteLine($"{base.Request.Method}: {base.Request.Url.Path}");
+
+            var request = this.Bind<Core.Domain.Freight.Request>();
+
+            this._notificationHubContext.Clients.All.push("Deleted", request);
+
+            return base.Response.AsJson(new { Success = true });
+        }
+    }
+}
---
+++ CL.Javelin.Sales.Service/Routes/Freight/RequestRoutes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using CL.Javelin.Core.Utilities;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace CL.Javelin.Sales.Service.Routes.Freight
+{
+    public class RequestRoutes : NancyModule
+    {
+        private const string BaseUrl = "/freight/requests";
+
+        public RequestRoutes()
+        {
+            base.Get[$"{BaseUrl}"] = this.GetFreightRequests;
+            base.Post[$"{BaseUrl}"] = this.CreateFreightRequest;
+        }
+
+        private async Task<dynamic> GetFreightRequests(dynamic parameters, CancellationToken ct)
+        {
+            Console.WriteLine($"{base.Request.Method}: {base.Request.Url.Path}");
+
+            // post to Store for creation
+            IEnumerable<Core.Domain.Freight.Request> requests = 
+                await Http.Get<IEnumerable<Core.Domain.Freight.Request>>("http://127.0.0.1:9000/freight/requests");
+
+            return requests;
+        }
+
+        private async Task<dynamic> CreateFreightRequest(dynamic parameters, CancellationToken ct)
+        {
+            Console.WriteLine($"{base.Request.Method}: {base.Request.Url.Path}");
+
+            var request = this.Bind<Core.Domain.Freight.Request>();
+
+            // post to Store for creation
+            var response = await Http.Post<Core.Domain.Freight.Request>("http://127.0.0.1:9000/freight/requests", request);
+            
+            return response.Content;
+        }
+    }
+}
