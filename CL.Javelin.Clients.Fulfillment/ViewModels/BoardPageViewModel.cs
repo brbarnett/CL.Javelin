458b8597fab06b4d12ab30210d9b4ccf7b5a5e78
Brian Bennewitz <BBennewitz@rightpoint.com>
2016-03-31 11:48:50 -0500

    Removed dead commands

--- CL.Javelin.Clients.Fulfillment/ViewModels/BoardPageViewModel.cs
+++ CL.Javelin.Clients.Fulfillment/ViewModels/BoardPageViewModel.cs
@@ -7,10 +7,6 @@ namespace CL.Javelin.Clients.Fulfillment.ViewModels
 {
     public class BoardPageViewModel : BoardPageViewModelBase
     {
-        public ICommand ToggleSelectedRequestOpenCommand { get; private set; }
-
-        public ICommand DeleteSelectedRequestCommand { get; private set; }
-
         public BoardPageViewModel(IEventAggregator eventAggregator)
             : base(eventAggregator)
         {
