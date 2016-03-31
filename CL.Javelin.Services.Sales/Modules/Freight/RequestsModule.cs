244eb60f23b3dff5020c3bc3ce177cbbcff92808
brbarnett <barnett@echosolutionsgroup.com>
2016-03-31 11:57:51 -0500

    Adding Put to Sales

--- CL.Javelin.Services.Sales/Modules/Freight/RequestsModule.cs
+++ CL.Javelin.Services.Sales/Modules/Freight/RequestsModule.cs
@@ -15,6 +15,7 @@ namespace CL.Javelin.Services.Sales.Modules.Freight
         public RequestsModule()
         {
             base.Get[$"{BaseUrl}"] = this.GetFreightRequests;
+            base.Put[$"{BaseUrl}"] = this.UpdateFreightRequest;
             base.Post[$"{BaseUrl}"] = this.CreateFreightRequest;
         }
 
@@ -26,6 +27,18 @@ namespace CL.Javelin.Services.Sales.Modules.Freight
                 await Http.Get<IEnumerable<Core.Domain.Freight.Request>>("http://127.0.0.1:9000/freight/requests");
 
             return base.Response.AsJson(requests);
+        }
+
+        private async Task<dynamic> UpdateFreightRequest(dynamic parameters, CancellationToken ct)
+        {
+            Console.WriteLine($"{base.Request.Method}: {base.Request.Url.Path}");
+
+            var request = this.Bind<Core.Domain.Freight.Request>();
+
+            // post to Store for creation
+            var response = await Http.Put<Core.Domain.Freight.Request>("http://127.0.0.1:9000/freight/requests", request);
+
+            return base.Response.AsJson(response.Content);
         }
 
         private async Task<dynamic> CreateFreightRequest(dynamic parameters, CancellationToken ct)
